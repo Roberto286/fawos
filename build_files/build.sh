f56bc644a2998110bb33c229a90cd334bcaf63d8
Roberto Saliola <roberto.saliola96@gmail.com>
2026-09-19 20:45:04 +0200

    add hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,16 +15,11 @@ cp -avf "/ctx/system_files"/. /
 # this installs a package from fedora repos
 dnf5 install -y tmux
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-useradd -m -G wheel -s /bin/bash t
-echo 't:t' | chpasswd
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y install hyprland xdg-desktop-portal-hyprland waybar kitty swww
+
+dnf5 -y copr disable solopasha/hyprland
 
