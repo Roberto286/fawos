5ff18426b9c9fe11f90952e534de6bbb2680c54e
roberto286 <roberto.saliola96@gmail.com>
2026-09-23 12:12:00 +0200

    refactor: build.sh becomes pure orchestrator sourcing lib+domains

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 set -ouex pipefail
+
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-# Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-source "${SCRIPT_DIR}/kernel.sh"
-source "${SCRIPT_DIR}/dms.sh"
+source "${SCRIPT_DIR}/lib/common.sh"
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://rpmfusion.org
-
-# this installs a package from fedora repos
-dnf5 install -y tmux
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
-
-
+for domain in users kernel desktop terminal devtools containers apps gpu gaming power; do
+  source "${SCRIPT_DIR}/domains/${domain}.sh"
+done
