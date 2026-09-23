0db9a46b5479fc802fbe90234dc693446903fbad
roberto286 <roberto.saliola96@gmail.com>
2026-09-23 12:04:08 +0200

    feat: add apps domain (Firefox, Bitwarden desktop via Flatpak)

---
+++ build_files/domains/apps.sh
@@ -0,0 +1,6 @@
+# --- APP GUI: FIREFOX & BITWARDEN (FLATPAK, BAKED A BUILD-TIME) ---
+dnf5 install -y flatpak
+flatpak remote-add --system --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --system -y flathub org.mozilla.firefox com.bitwarden.desktop
+
+# --- FINE APP GUI ---
