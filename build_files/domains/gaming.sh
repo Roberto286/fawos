d90461b16def844fd336db90569a35fb03097770
roberto286 <roberto.saliola96@gmail.com>
2026-09-23 12:06:56 +0200

    feat: add gaming domain (Steam RPM, Lutris, GameMode, MangoHud)

---
+++ build_files/domains/gaming.sh
@@ -0,0 +1,8 @@
+# --- GAMING: RPM FUSION (persistente) + STEAM + LUTRIS + GAMEMODE/MANGOHUD ---
+FEDORA_VERSION=$(rpm -E %fedora)
+enable_persistent_repo "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${FEDORA_VERSION}.noarch.rpm"
+enable_persistent_repo "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${FEDORA_VERSION}.noarch.rpm"
+
+install_packages "${SCRIPT_DIR}/packages/gaming.txt"
+
+# --- FINE GAMING ---
